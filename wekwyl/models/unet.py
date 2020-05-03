9e8b4d0a4ea72b43a02c5cb263d03b3d9a0ab3a2
ploshkin <ploshkin@yandex-team.ru>
2020-05-04 01:33:04 +0500

    change unet architecture.

--- wekwyl/models/unet.py
+++ wekwyl/models/unet.py
@@ -2,88 +2,100 @@ import functools
 
 import torch as th
 import torch.nn as nn
+import torch.nn.functional as F
 
 from .conv import CylindricConv2d
 
 
-class CylindircUnetNN(nn.Module):
-    """Create a Unet-based generator"""
+class _UnetBlock(nn.Module):
+    """Defines the Unet submodule with skip connection.
+        X -------------------identity----------------------
+        |-- downsampling -- |submodule| -- upsampling --|
+    """
 
     def __init__(
             self,
-            input_nc,
-            output_nc,
-            num_downs,
-            ngf=64,
+            n_out,
+            n_flt,
+            n_in=None,
+            subnet=None,
+            outer=False,
+            inner=False,
             norm_layer=nn.BatchNorm2d,
             use_dropout=False,
+            kernel_sizes=[(3, 3),]
     ):
-        """Construct a Unet generator
+        """Construct a Unet submodule with skip connections.
         Parameters:
-            input_nc (int)  -- the number of channels in input images
-            output_nc (int) -- the number of channels in output images
-            num_downs (int) -- the number of downsamplings in UNet. For example, # if |num_downs| == 7,
-                               image of size 128x128 will become of size 1x1 # at the bottleneck
-            ngf (int)       -- the number of filters in the last conv layer
-            norm_layer      -- normalization layer
-        We construct the U-Net from the innermost layer to the outermost layer.
-        It is a recursive process.
+            n_out (int) -- the number of filters in the outer conv layer
+            n_flt (int) -- the number of filters in the inner conv layer
+            n_in (int) -- the number of channels in input images/features
+            subnet (_UnetBlock) -- previously defined submodules
+            outer (bool) -- if this module is the outermost module
+            inner (bool) -- if this module is the innermost module
+            norm_layer -- normalization layer
+            use_dropout (bool) -- if use dropout layers
+            kernel_sizes (list) -- list of kernel sizes inside CylindricConv2d
         """
-        super(CylindircUnetNN, self).__init__()
-        # Add the innermost layer.
-        unet_block = UnetNNSkipConnectionBlock(
-            ngf * 8,
-            ngf * 8,
-            input_nc=None,
-            submodule=None,
-            norm_layer=norm_layer,
-            innermost=True,
-        )
-        # Add intermediate layers with ngf * 8 filters.
-        for i in range(num_downs - 5):
-            unet_block = UnetNNSkipConnectionBlock(
-                ngf * 8,
-                ngf * 8,
-                input_nc=None,
-                submodule=unet_block,
-                norm_layer=norm_layer,
-                use_dropout=use_dropout,
-            )
-        # Gradually reduce the number of filters from ngf * 8 to ngf.
-        unet_block = UnetNNSkipConnectionBlock(
-            ngf * 4,
-            ngf * 8,
-            input_nc=None,
-            submodule=unet_block,
-            norm_layer=norm_layer,
-        )
-        unet_block = UnetNNSkipConnectionBlock(
-            ngf * 2,
-            ngf * 4,
-            input_nc=None,
-            submodule=unet_block,
-            norm_layer=norm_layer,
-        )
-        unet_block = UnetNNSkipConnectionBlock(
-            ngf,
-            ngf * 2,
-            input_nc=None,
-            submodule=unet_block,
-            norm_layer=norm_layer,
-        )
-        # Add the outermost layer.
-        self.model = UnetNNSkipConnectionBlock(
-            output_nc,
-            ngf,
-            input_nc=input_nc,
-            submodule=unet_block,
-            outermost=True,
-            norm_layer=norm_layer,
-        )
+        super(UnetBlock, self).__init__()
+        self.outer = outer
+
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        if not n_in:
+            n_in = n_out
+
+        if outer:
+            layers = [
+                CylindricConv2d(n_in, n_flt, kernel_sizes, stride=2, bias=use_bias),
+                norm_layer(n_flt),
+                subnet,
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.UpsamplingNearest2d(scale_factor=2),
+                CylindricConv2d(n_flt * 2, n_flt, kernel_sizes, bias=use_bias),
+                norm_layer(n_flt),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(n_flt, n_out, (1, 1), bias=use_bias),
+                norm_layer(n_out),
+                nn.Sigmoid(),
+            ]
+
+        elif inner:
+            layers = [
+                nn.LeakyReLU(0.2, inplace=True),
+                CylindricConv2d(n_in, n_flt, kernel_sizes, stride=2, bias=use_bias),
+                norm_layer(n_flt),
+                nn.ReLU(inplace=True),
+                nn.UpsamplingNearest2d(scale_factor=2),
+                CylindricConv2d(n_flt, n_out, kernel_sizes, bias=use_bias),
+                norm_layer(n_out),
+            ]
+
+        else:
+            layers = [
+                nn.LeakyReLU(0.2, inplace=True),
+                CylindricConv2d(n_in, n_flt, kernel_sizes, stride=2, bias=use_bias),
+                norm_layer(n_flt),
+                subnet,
+                nn.ReLU(inplace=True),
+                nn.UpsamplingNearest2d(scale_factor=2),
+                CylindricConv2d(n_flt * 2, n_out, kernel_sizes, bias=use_bias),
+                norm_layer(n_out),
+            ]
+
+            if use_dropout:
+                layers.append(nn.Dropout(0.5))
+
+        self.model = nn.Sequential(*layers)
 
     def forward(self, x):
-        """Standard forward"""
-        return self.model(x)
+        if self.outer:
+            return self.model(x)
+        else:   # Add skip connection.
+            return th.cat([x, self.model(x)], dim=1)
 
     def to(self, *args, **kwargs):
         self = super().to(*args, **kwargs)
@@ -93,103 +105,67 @@ class CylindircUnetNN(nn.Module):
         return self
 
 
-class UnetNNSkipConnectionBlock(nn.Module):
-    """Defines the Unet submodule with skip connection.
-        X -------------------identity----------------------
-        |-- downsampling -- |submodule| -- upsampling --|
-    """
+class CylindricUnet(nn.Module):
+
+    """Create a Unet-based generator"""
 
     def __init__(
             self,
-            outer_nc,
-            inner_nc,
-            input_nc=None,
-            submodule=None,
-            outermost=False,
-            innermost=False,
+            n_in,
+            n_out,
+            n_downs,
+            ngf=64,
             norm_layer=nn.BatchNorm2d,
             use_dropout=False,
-            kernel_sizes=[(3, 3),]
     ):
-        """Construct a Unet submodule with skip connections.
+        """Construct a Unet generator
         Parameters:
-            outer_nc (int) -- the number of filters in the outer conv layer
-            inner_nc (int) -- the number of filters in the inner conv layer
-            input_nc (int) -- the number of channels in input images/features
-            submodule (UnetSkipConnectionBlock) -- previously defined submodules
-            outermost (bool)    -- if this module is the outermost module
-            innermost (bool)    -- if this module is the innermost module
-            norm_layer          -- normalization layer
-            user_dropout (bool) -- if use dropout layers.
-            kernel_sizes (list) -- list of kernel sizes inside CylindricConv2d
+            n_in (int) -- the number of channels in input images
+            n_out (int) -- the number of channels in output images
+            n_downs (int) -- the number of downsamplings in UNet. For example, if |n_downs| == 7,
+                             image of size 128x128 will become of size 1x1 at the bottleneck
+            ngf (int) -- the number of filters in the last conv layer
+            norm_layer -- normalization layer
+        We construct the U-Net from the innermost layer to the outermost layer.
+        It is a recursive process.
         """
-        super(UnetNNSkipConnectionBlock, self).__init__()
-        self.outermost = outermost
-
-        if type(norm_layer) == functools.partial:
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
-        if input_nc is None:
-            input_nc = outer_nc
-
-        downconv = CylindricConv2d(
-            input_nc,
-            inner_nc,
-            kernel_sizes=kernel_sizes,
-            stride=2,
-            bias=use_bias,
+        super(CylindricUnet, self).__init__()
+        # Add the innermost layer.
+        subnet = UnetBlock(
+            ngf * 8, ngf * 8, norm_layer=norm_layer, inner=True,
         )
-        downrelu = nn.LeakyReLU(0.2, True)
-        downnorm = norm_layer(inner_nc)
-        uprelu = nn.ReLU(True)
-        upnorm = norm_layer(outer_nc)
-        upscale = nn.UpsamplingNearest2d(scale_factor=2)
-
-        if outermost:
-            updownconv = CylindricConv2d(
-                inner_nc * 2,
-                outer_nc,
-                kernel_sizes=kernel_sizes,
-                bias=use_bias,
+        # Add intermediate layers with ngf * 8 filters.
+        for i in range(n_downs - 5):
+            subnet = UnetBlock(
+                ngf * 8,
+                ngf * 8,
+                subnet=subnet,
+                norm_layer=norm_layer,
+                use_dropout=use_dropout,
             )
-            down = [downconv]
-            up = [uprelu, upscale, updownconv, nn.Sigmoid()]
-            model = down + [submodule] + up
-
-        elif innermost:
-            updownconv = CylindricConv2d(
-                inner_nc,
-                outer_nc,
-                kernel_sizes=kernel_sizes,
-                bias=use_bias,
-            )
-            down = [downrelu, downconv]
-            up = [uprelu, upscale, updownconv, upnorm]
-            model = down + up
-
-        else:
-            updownconv = CylindricConv2d(
-                inner_nc * 2,
-                outer_nc,
-                kernel_sizes=kernel_sizes,
-                bias=use_bias,
-            )
-            down = [downrelu, downconv, downnorm]
-            up = [uprelu, upscale, updownconv, upnorm]
-
-            if use_dropout:
-                model = down + [submodule] + up + [nn.Dropout(0.5)]
-            else:
-                model = down + [submodule] + up
-
-        self.model = nn.Sequential(*model)
+        # Gradually reduce the number of filters from ngf * 8 to ngf.
+        subnet = UnetBlock(
+            ngf * 4, ngf * 8, subnet=subnet, norm_layer=norm_layer,
+        )
+        subnet = UnetBlock(
+            ngf * 2, ngf * 4, subnet=subnet, norm_layer=norm_layer,
+        )
+        subnet = UnetBlock(
+            ngf, ngf * 2, subnet=subnet, norm_layer=norm_layer,
+        )
+        # Add the outermost layer.
+        self.model = UnetBlock(
+            n_out,
+            ngf,
+            n_in=n_in,
+            subnet=subnet,
+            outer=True,
+            norm_layer=norm_layer,
+        )
 
     def forward(self, x):
-        if self.outermost:
-            return self.model(x)
-        else:   # Add skip connection.
-            return th.cat([x, self.model(x)], dim=1)
+        """Standard forward"""
+        return self.model(x)
 
     def to(self, *args, **kwargs):
         self = super().to(*args, **kwargs)
