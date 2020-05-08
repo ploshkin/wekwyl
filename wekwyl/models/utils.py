51b5a249f57475669aa32a4fd4438539a3a42b5f
ploshkin <ploshkin@yandex-team.ru>
2020-05-08 17:05:46 +0500

    add none normalization.

--- wekwyl/models/utils.py
+++ wekwyl/models/utils.py
@@ -3,6 +3,15 @@ import functools
 import torch.nn as nn
 
 
+class Identity(nn.Module):
+
+    def __init__(self, *args, **kwargs):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
 def get_norm_layer(norm_type='instance'):
     """Return a normalization layer
     Parameters:
@@ -15,7 +24,7 @@ def get_norm_layer(norm_type='instance'):
     elif norm_type == 'instance':
         norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
     elif norm_type == 'none':
-        norm_layer = None
+        norm_layer = Identity
     else:
         raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
     return norm_layer
